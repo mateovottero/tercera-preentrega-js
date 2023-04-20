c464980b58a475b8b836ff42733713ff4d418cf4
mateovottero <mateo.vottero1@gmail.com>
2023-04-20 16:54:57 -0300

    first commit

---
+++ data/stock.js
@@ -0,0 +1,90 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Intel Core I9",
+        tipo: "buzo",
+        desc: "I9 10900K LGA1200",
+        precio: 335200,
+        imagen: '../public/images/procesador.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "RTX 4070",
+        tipo: "grafica",
+        desc: "GeForce RTX 4070 TufGaming",
+        precio: 450000,
+        imagen: '../public/images/grafica.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Teclado",
+        tipo: "HyperX",
+        desc: "HyperX Alloy FPS",
+        precio: 29900,
+        imagen: '../public/images/teclado.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Mouse",
+        tipo: "Mouse HyperX",
+        desc: "Mouse HyperX FPS Pro",
+        precio: 16000,
+        imagen: '../public/images/mouse.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Mouse Pad",
+        tipo: "pad",
+        desc: "Mouse Pad HyperX speed tamaño L",
+        precio: 12000,
+        imagen: '../public/images/pad.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Auriculares",
+        tipo: "auriculares",
+        desc: "auriculares HyperX Cloud II",
+        precio: 50000,
+        imagen: '../public/images/auriculares.jpg',
+        cantidad:1
+    },
+    {
+        id: 7,
+        nombre: "Monitor",
+        tipo: "MonitorViewsonic",
+        desc: "24' pulgadas 165Hz",
+        precio: 180000,
+        imagen: '../public/images/monitor.jpg',
+        cantidad:1
+    },
+    {
+        id: 8,
+        nombre: "Placa Madre",
+        tipo: "Placa Madre",
+        desc: "Gigabyte B550 Aourus DDR4",
+        precio: 72000,
+        imagen: '../public/images/mother.jpg',
+        cantidad:1
+    },
+    {
+        id: 9,
+        nombre: "Gabinete",
+        tipo: "Gabinete Aourus",
+        desc: "ATX C300 GLASS",
+        precio: 64000,
+        imagen: '../public/images/gabinete.jpg',
+        cantidad:1
+    }
+
+];
